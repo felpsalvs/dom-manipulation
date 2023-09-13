55a9ad1ab05bd5b0bc5024e7eb51e306eeaaa382
Felipe Alves <felipe.alves@svm.com>
2023-09-13 17:15:58 -0300

    add colors

--- src/main.js
+++ src/main.js
@@ -1,2 +1,20 @@
 import "./css/index.css"
 
+const ccBgColor01 = document.querySelector(".cc-bg svg > g g:nth-child(1) path")
+const ccBgColor02 = document.querySelector(".cc-bg svg > g g:nth-child(2) path")
+const ccLogo = document.querySelector(".cc-logo span:nth-child(2) img")
+
+function setCardType(type) {
+  const colors = {
+    visa: ["#436d99", "#2d57f2"],
+    mastercard:["#e8b2c8","#ff5da6"] ,
+    rocketseat: ["#0d6f5d", "#c3129c"],
+    default: ["black", "gray"]
+  }
+
+  ccBgColor01.setAttribute("fill", colors[type][0])
+  ccBgColor02.setAttribute("fill", colors[type][1])
+  ccLogo.setAttribute("src", `cc-${type}.svg`)
+}
+
+globalThis.setCardType = setCardType
