cfa0d02511d14fea214c2be116195479a3eee150
Felipe Alves <felipe.alves@svm.com>
2023-09-14 14:00:58 -0300

    update main.js

--- src/main.js
+++ src/main.js
@@ -43,4 +43,4 @@ const expirationDatePattern = {
   }
 }
 
-const expirationDateMasked = IMask(expirationDate, expirationDatePattern)
+//const expirationDateMasked = IMask(expirationDate, expirationDatePattern)
